1042a1ef4ec86a162335983c3cf23764a3d8d5cf
alphaqu <yan.gyunashyan@gmail.com>
2022-08-06 21:15:41 +0200

    feat: Contains and overlaps

--- src/types/rect.rs
+++ src/types/rect.rs
@@ -1,138 +1,156 @@
+use std::cmp::Ordering;
 use crate::impl_ops;
 use crate::number::Number;
 use crate::types::vec2::Vec2D;
 use crate::unit::{check_compatible, Unit, UnitCompatibility};
-use std::cmp::Ordering;
 
 #[derive(Copy, Clone, Debug)]
 #[repr(C)]
 pub struct Rect<T: Number, U: Unit = ()> {
-    pub(crate) origin: [T; 2],
-    pub(crate) size: [T; 2],
-    pub(crate) unit: U,
+	pub(crate) origin: [T; 2],
+	pub(crate) size: [T; 2],
+	pub(crate) unit: U,
 }
 
 impl<N: Number> Rect<N> {
-    pub fn new(origin: impl Into<[N; 2]>, size: impl Into<[N; 2]>) -> Rect<N> {
-        Rect::new_u(origin, size, ())
-    }
+	pub fn new(origin: impl Into<[N; 2]>, size: impl Into<[N; 2]>) -> Rect<N> {
+		Rect::new_u(origin, size, ())
+	}
 
-    pub fn new_min_max(min: impl Into<[N; 2]>, max: impl Into<[N; 2]>) -> Rect<N> {
-        Rect::new_min_max_u(min, max, ())
-    }
+	pub fn new_min_max(min: impl Into<[N; 2]>, max: impl Into<[N; 2]>) -> Rect<N> {
+		Rect::new_min_max_u(min, max, ())
+	}
 
-    pub fn zero() -> Rect<N> {
-        Rect::new(Vec2D::zero(), Vec2D::zero())
-    }
+	pub fn zero() -> Rect<N> {
+		Rect::new(Vec2D::zero(), Vec2D::zero())
+	}
 
-    pub fn one() -> Rect<N> {
-        Rect::new(Vec2D::one(), Vec2D::one())
-    }
+	pub fn one() -> Rect<N> {
+		Rect::new(Vec2D::one(), Vec2D::one())
+	}
 }
 
 impl<N: Number, U: Unit> Rect<N, U> {
-    pub fn new_u(origin: impl Into<[N; 2]>, size: impl Into<[N; 2]>, unit: U) -> Rect<N, U> {
-        Rect {
-            origin: origin.into(),
-            size: size.into(),
-            unit,
-        }
-    }
+	pub fn new_u(origin: impl Into<[N; 2]>, size: impl Into<[N; 2]>, unit: U) -> Rect<N, U> {
+		Rect {
+			origin: origin.into(),
+			size: size.into(),
+			unit,
+		}
+	}
 
-    pub fn new_min_max_u(min: impl Into<[N; 2]>, max: impl Into<[N; 2]>, unit: U) -> Rect<N, U> {
-        let max = max.into();
-        let min = min.into();
-        Rect {
-            origin: min,
-            size: [
-                max[0] - min[0],
-                max[1] - min[1]
-            ],
-            unit,
-        }
-    }
+	pub fn new_min_max_u(min: impl Into<[N; 2]>, max: impl Into<[N; 2]>, unit: U) -> Rect<N, U> {
+		let max = max.into();
+		let min = min.into();
+		Rect {
+			origin: min,
+			size: [
+				max[0] - min[0],
+				max[1] - min[1]
+			],
+			unit,
+		}
+	}
 
-    #[inline(always)]
-    pub fn cast<NO: Number>(self) ->  Option<Rect<NO, U>> {
-        Some(Rect {
-            origin: [
-                NO::from(self.origin[0])?,
-                NO::from(self.origin[1])?
-            ],
-            size: [
-                NO::from(self.size[0])?,
-                NO::from(self.size[1])?
-            ],
-            unit: self.unit,
-        })
-    }
+	#[inline(always)]
+	pub fn cast<NO: Number>(self) -> Option<Rect<NO, U>> {
+		Some(Rect {
+			origin: [
+				NO::from(self.origin[0])?,
+				NO::from(self.origin[1])?
+			],
+			size: [
+				NO::from(self.size[0])?,
+				NO::from(self.size[1])?
+			],
+			unit: self.unit,
+		})
+	}
 
-    #[inline(always)]
-    pub fn convert_def<UO: UnitCompatibility<N, U> + Default>(self) ->  Option<Rect<N, UO>> {
-        self.convert_u(UO::default())
-    }
+	#[inline(always)]
+	pub fn convert_def<UO: UnitCompatibility<N, U> + Default>(self) -> Option<Rect<N, UO>> {
+		self.convert_u(UO::default())
+	}
 
-    #[inline(always)]
-    pub fn convert_u<UO: UnitCompatibility<N, U>>(self, to: UO) -> Option<Rect<N, UO>> {
-        to.convert_rect(self)
-    }
+	#[inline(always)]
+	pub fn convert_u<UO: UnitCompatibility<N, U>>(self, to: UO) -> Option<Rect<N, UO>> {
+		to.convert_rect(self)
+	}
 
-    pub fn contains_pos(&self, pos: Vec2D<N, U>) -> bool {
-        let s_min = self.min();
-        let s_max = self.max();
-        s_min.x() <= pos.x() && pos.x() < s_max.x() && s_min.y() <= pos.y() && pos.y() < s_max.y()
-    }
+	pub fn overlap_rect(&self, other: Rect<N, U>) -> bool {
+		self.contains_pos(other.min()) || self.contains_pos(other.max())
+	}
 
-    pub fn contains_rect(&self, rect: Rect<N, U>) -> bool {
-        let s_min = self.min();
-        let s_max = self.max();
-        let o_min = rect.min();
-        let o_max = rect.max();
-        s_min.x() <= o_min.x()
-            && o_max.x() <= s_max.x()
-            && s_min.y() <= o_min.y()
-            && o_max.y() <= s_max.y()
-    }
+	pub fn contains_rect(&self, rect: Rect<N, U>) -> bool {
+		self.contains_pos(rect.min()) && self.contains_pos(rect.max())
+	}
 
-    #[inline(always)]
-    pub fn any_unit(self) -> Rect<N, ()> {
-        Rect {
-            origin: self.origin,
-            size: self.size,
-            unit: (),
-        }
-    }
+	pub fn contains_pos(&self, pos: Vec2D<N, U>) -> bool {
+		let s_min = self.min();
+		let s_max = self.max();
+		s_min.x() <= pos.x() && pos.x() <= s_max.x() && s_min.y() <= pos.y() && pos.y() <= s_max.y()
+	}
 
-    #[inline(always)]
-    pub fn unit(self) -> U {
-        self.unit
-    }
 
-    #[inline(always)]
-    pub fn min(self) -> Vec2D<N, U> {
-        self.origin()
-    }
+	#[inline(always)]
+	pub fn any_unit(self) -> Rect<N, ()> {
+		Rect {
+			origin: self.origin,
+			size: self.size,
+			unit: (),
+		}
+	}
 
-    #[inline(always)]
-    pub fn max(self) -> Vec2D<N, U> {
-        self.origin() + self.size()
-    }
+	#[inline(always)]
+	pub fn unit(self) -> U {
+		self.unit
+	}
 
-    #[inline(always)]
-    pub fn origin(self) -> Vec2D<N, U> {
-        Vec2D {
-            value: self.origin,
-            unit: self.unit,
-        }
-    }
+	#[inline(always)]
+	pub fn top_left(self) -> Vec2D<N, U> {
+		self.origin()
+	}
 
-    #[inline(always)]
-    pub fn size(self) -> Vec2D<N, U> {
-        Vec2D {
-            value: self.size,
-            unit: self.unit,
-        }
-    }
+	#[inline(always)]
+	pub fn top_right(self) -> Vec2D<N, U> {
+		self.origin() + Vec2D::new_u(self.size[0], N::zero(), self.unit)
+	}
+
+	#[inline(always)]
+	pub fn bottom_right(self) -> Vec2D<N, U> {
+		self.origin() + Vec2D::new_u(self.size[0], self.size[1], self.unit)
+	}
+
+	#[inline(always)]
+	pub fn bottom_left(self) -> Vec2D<N, U> {
+		self.origin() + Vec2D::new_u(N::zero(), self.size[1], self.unit)
+	}
+
+	#[inline(always)]
+	pub fn min(self) -> Vec2D<N, U> {
+		self.origin()
+	}
+
+	#[inline(always)]
+	pub fn max(self) -> Vec2D<N, U> {
+		self.origin() + self.size()
+	}
+
+	#[inline(always)]
+	pub fn origin(self) -> Vec2D<N, U> {
+		Vec2D {
+			value: self.origin,
+			unit: self.unit,
+		}
+	}
+
+	#[inline(always)]
+	pub fn size(self) -> Vec2D<N, U> {
+		Vec2D {
+			value: self.size,
+			unit: self.unit,
+		}
+	}
 }
 
 macro_rules! impl_op {
@@ -197,25 +215,49 @@ macro_rules! impl_op {
 impl_ops!(impl_op);
 
 impl<N: Number, U: Unit> PartialEq<Self> for Rect<N, U> {
-    fn eq(&self, other: &Self) -> bool {
-        self.origin == other.origin && self.size == other.size
-    }
+	fn eq(&self, other: &Self) -> bool {
+		self.origin == other.origin && self.size == other.size
+	}
 }
 
 impl<N: Number, U: Unit> Eq for Rect<N, U> {}
 
 impl<N: Number, U: Unit> PartialOrd<Self> for Rect<N, U> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let origin = self.origin().partial_cmp(&other.origin())?;
-        let size = self.size().partial_cmp(&other.size())?;
-        origin.partial_cmp(&size)
-    }
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		let origin = self.origin().partial_cmp(&other.origin())?;
+		let size = self.size().partial_cmp(&other.size())?;
+		origin.partial_cmp(&size)
+	}
 }
 
 impl<N: Number + Ord, U: Unit> Ord for Rect<N, U> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let origin = self.origin().cmp(&other.origin());
-        let size = self.size().cmp(&other.size());
-        origin.cmp(&size)
-    }
+	fn cmp(&self, other: &Self) -> Ordering {
+		let origin = self.origin().cmp(&other.origin());
+		let size = self.size().cmp(&other.size());
+		origin.cmp(&size)
+	}
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+	#[test]
+	fn contains() {
+		let rect = Rect::new([0.0f32, 0.0], [1.0, 1.0]);
+		assert!(rect.contains_rect(Rect::new([0.0f32, 0.0], [1.0, 1.0])));
+		assert!(rect.contains_rect(Rect::new([0.1f32, 0.1], [0.8, 0.8])));
+		assert!(!rect.contains_rect(Rect::new([0.1f32, 0.1], [1.0, 1.0])));
+		assert!(!rect.contains_rect(Rect::new([-0.1, -0.1], [1.1, 1.1])));
+	}
+
+	#[test]
+	fn overlaps() {
+		let rect = Rect::new([0.0f32, 0.0], [1.0, 1.0]);
+		assert!(rect.overlap_rect(Rect::new([0.0f32, 0.0], [1.0, 1.0])));
+		assert!(rect.overlap_rect(Rect::new([0.1f32, 0.1], [0.8, 0.8])));
+		assert!(rect.overlap_rect(Rect::new([0.1f32, 0.1], [1.0, 1.0])));
+		assert!(rect.overlap_rect(Rect::new([-0.1f32, -0.1f32], [0.1, 0.1])));
+		assert!(!rect.overlap_rect(Rect::new([1.11f32, 1.11], [1.0, 1.0])));
+	}
 }
--- src/types/vec2.rs
+++ src/types/vec2.rs
@@ -9,136 +9,148 @@ use crate::types::value::Value;
 #[derive(Copy, Clone, Debug)]
 #[repr(C)]
 pub struct Vec2D<T: Number, U: Unit = ()> {
-    pub(crate) value: [T; 2],
-    pub(crate) unit: U,
+	pub(crate) value: [T; 2],
+	pub(crate) unit: U,
 }
 
 impl<T: Number> Vec2D<T> {
-    pub fn new(x: T, y: T) -> Vec2D<T> {
-        Vec2D::new_u(x, y, ())
-    }
+	pub fn new(x: T, y: T) -> Vec2D<T> {
+		Vec2D::new_u(x, y, ())
+	}
 
-    pub fn zero() -> Vec2D<T> {
-        Vec2D::new(T::zero(), T::zero())
-    }
+	pub fn zero() -> Vec2D<T> {
+		Vec2D::new(T::zero(), T::zero())
+	}
 
-    pub fn one() -> Vec2D<T> {
-        Vec2D::new(T::one(), T::one())
-    }
+	pub fn one() -> Vec2D<T> {
+		Vec2D::new(T::one(), T::one())
+	}
 }
 
 
 impl<T: Number, U: Unit + Default> Vec2D<T, U> {
-    pub fn new_def(x: T, y: T) -> Vec2D<T, U> {
-        Vec2D::new_u(x, y, U::default())
-    }
+	pub fn new_def(x: T, y: T) -> Vec2D<T, U> {
+		Vec2D::new_u(x, y, U::default())
+	}
 }
 
 impl<N: Number, U: Unit> Vec2D<N, U> {
-    pub fn new_u(x: N, y: N, unit: U) -> Vec2D<N, U> {
-        Vec2D {
-            value: [x, y],
-            unit
-        }
-    }
+	pub fn new_u(x: N, y: N, unit: U) -> Vec2D<N, U> {
+		Vec2D {
+			value: [x, y],
+			unit,
+		}
+	}
 
-    #[inline(always)]
-    pub fn cast<NO: Number>(self) ->  Option<Vec2D<NO, U>> {
-       Some(Vec2D {
-           value: [
-               NO::from(self.value[0])?,
-               NO::from(self.value[1])?
-           ],
-           unit: self.unit
-       })
-    }
+	#[inline(always)]
+	pub fn cast<NO: Number>(self) -> Option<Vec2D<NO, U>> {
+		Some(Vec2D {
+			value: [
+				NO::from(self.value[0])?,
+				NO::from(self.value[1])?
+			],
+			unit: self.unit,
+		})
+	}
 
-    #[inline(always)]
-    pub fn convert_def<UO: UnitCompatibility<N, U> + Default>(self) ->  Option<Vec2D<N, UO>> {
-        self.convert_u(UO::default())
-    }
+	#[inline(always)]
+	pub fn convert_def<UO: UnitCompatibility<N, U> + Default>(self) -> Option<Vec2D<N, UO>> {
+		self.convert_u(UO::default())
+	}
 
-    #[inline(always)]
-    pub fn convert_u<UO: UnitCompatibility<N, U>>(self, to: UO) -> Option<Vec2D<N, UO>> {
-        to.convert_pos2(self)
-    }
+	#[inline(always)]
+	pub fn convert_u<UO: UnitCompatibility<N, U>>(self, to: UO) -> Option<Vec2D<N, UO>> {
+		to.convert_pos2(self)
+	}
 
-    #[inline(always)]
-    pub fn any_unit(self) -> Vec2D<N, ()> {
-        Vec2D {
-            value: self.value,
-            unit: ()
-        }
-    }
+	#[inline(always)]
+	pub fn any_unit(self) -> Vec2D<N, ()> {
+		Vec2D {
+			value: self.value,
+			unit: (),
+		}
+	}
 
-    #[inline(always)]
-    pub fn unit(self) -> U {
-        self.unit
-    }
+	#[inline(always)]
+	pub fn unit(self) -> U {
+		self.unit
+	}
 
-    #[inline(always)]
-    pub fn x(self) -> Value<N, U> {
-        Value::new(self.value[0], self.unit)
-    }
+	#[inline(always)]
+	pub fn x(self) -> Value<N, U> {
+		Value::new(self.value[0], self.unit)
+	}
 
-    #[inline(always)]
-    pub fn y(self) -> Value<N, U> {
-        Value::new(self.value[1], self.unit)
-    }
+	#[inline(always)]
+	pub fn y(self) -> Value<N, U> {
+		Value::new(self.value[1], self.unit)
+	}
 
-    #[inline(always)]
-    pub fn xy(self) -> Vec2D<N, U> {
-        self
-    }
+	#[inline(always)]
+	pub fn xy(self) -> Vec2D<N, U> {
+		self
+	}
 
-    #[inline(always)]
-    pub fn yx(self) -> Vec2D<N, U> {
-        Vec2D {
-            value: [
-                self.value[1],
-                self.value[0],
-            ],
-            unit: self.unit
-        }
-    }
+	#[inline(always)]
+	pub fn yx(self) -> Vec2D<N, U> {
+		Vec2D {
+			value: [
+				self.value[1],
+				self.value[0],
+			],
+			unit: self.unit,
+		}
+	}
 }
 
 impl<N: Number, U: Unit> Into<[N; 2]> for Vec2D<N, U> {
-    fn into(self) -> [N; 2] {
-        self.value
-    }
+	fn into(self) -> [N; 2] {
+		self.value
+	}
 }
 
 impl<N: Number, U: Unit> Into<(N, N)> for Vec2D<N, U> {
-    fn into(self) -> (N, N) {
-        (self.value[0], self.value[1])
-    }
+	fn into(self) -> (N, N) {
+		(self.value[0], self.value[1])
+	}
 }
 
 impl<N: Number, U: Unit> PartialEq<Self> for Vec2D<N, U> {
-    fn eq(&self, other: &Self) -> bool {
-        self.value == other.value
-    }
+	fn eq(&self, other: &Self) -> bool {
+		self.value == other.value
+	}
 }
 
-impl<N: Number, U: Unit> Eq for Vec2D<N, U> {
-
-}
+impl<N: Number, U: Unit> Eq for Vec2D<N, U> {}
 
 impl<N: Number, U: Unit> PartialOrd<Self> for Vec2D<N, U> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        let x = self.value[0].partial_cmp(&other.value[0])?;
-        let y = self.value[1].partial_cmp(&other.value[1])?;
-        x.partial_cmp(&y)
-    }
+	fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+		let x = self.value[0].partial_cmp(&other.value[0])?;
+		let y = self.value[1].partial_cmp(&other.value[1])?;
+		let ordering = x as i8 + y as i8;
+		if ordering > 0 {
+			Some(Ordering::Greater)
+		} else if ordering < 0 {
+			Some(Ordering::Less)
+		} else {
+			Some(Ordering::Equal)
+		}
+	}
 }
 
 impl<N: Number + Ord, U: Unit> Ord for Vec2D<N, U> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let x = self.value[0].cmp(&other.value[0]);
-        let y = self.value[1].cmp(&other.value[1]);
-        x.cmp(&y)
-    }
+	fn cmp(&self, other: &Self) -> Ordering {
+		let x = self.value[0].cmp(&other.value[0]);
+		let y = self.value[1].cmp(&other.value[1]);
+		let ordering = x as i8 + y as i8;
+		if ordering > 0 {
+			Ordering::Greater
+		} else if ordering < 0 {
+			Ordering::Less
+		} else {
+			Ordering::Equal
+		}
+	}
 }
 
 
@@ -185,14 +197,14 @@ impl_ops!(impl_op);
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+	use super::*;
 
-    #[test]
-    fn add() {
-        let v0 = Vec2D::new(0.5, 0.5);
-        let v1 = Vec2D::new(1.0, 1.0);
+	#[test]
+	fn add() {
+		let v0 = Vec2D::new(0.5, 0.5);
+		let v1 = Vec2D::new(1.0, 1.0);
 
-        assert_eq!(v0 + v1, Vec2D::new(1.5, 1.5));
-        assert_eq!(v0 + 2.0, Vec2D::new(2.5, 2.5));
-    }
+		assert_eq!(v0 + v1, Vec2D::new(1.5, 1.5));
+		assert_eq!(v0 + 2.0, Vec2D::new(2.5, 2.5));
+	}
 }
